3b21423de456a669d58123a8030cf764acf4459e
jrodriguezrios <72474060+jrodriguezrios@users.noreply.github.com>
2026-06-04 17:46:30 -0500

    Update slack.js

--- api/slack.js
+++ api/slack.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).end();
 
-  const WEBHOOK = 'https://hooks.slack.com/services/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';
+  const WEBHOOK = 'https://hooks.slack.com/services/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';
 
   try {
     let body = req.body;
