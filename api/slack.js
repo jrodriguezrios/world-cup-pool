28e78d43a80acc7e646c80b03800b5557c9dc191
jrodriguezrios <72474060+jrodriguezrios@users.noreply.github.com>
2026-06-04 16:43:00 -0500

    Update Slack webhook URL

--- api/slack.js
+++ api/slack.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).end();
 
-  const WEBHOOK = 'https://hooks.slack.com/services/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';
+  const WEBHOOK = 'https://hooks.slack.com/services/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';
 
   try {
     const response = await fetch(WEBHOOK, {
